e99cf2ce4b16dcfc40957ed7dc1d469b814b9e11
Jhenja Fedko <fedkozhenja@gmail.com>
2023-05-06 00:24:19 +0400

    feat: add a solution to the second task

---
+++ 02-write-file/index.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+const path = require('path');
+
+fs.writeFile(
+  path.join(__dirname, 'text.txt'),
+  '',
+  (err) => {
+    if (err) throw err;
+  });
+
+const { stdin, stdout, exit } = process;
+
+stdout.write('Введите текст\n');
+stdin.on('data', data => {
+  const text = data.toString().trim();
+
+  if (text === 'exit') {
+    exit();
+  } else {
+    fs.appendFile(
+      path.join(__dirname, 'text.txt'),
+      data,
+      err => {
+        if (err) throw err;
+      }
+    );
+  }
+});
+
+process.on('exit', () => stdout.write('Удачи!'));
